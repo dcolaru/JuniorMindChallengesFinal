5f33ec2510b0646ce06fbe3701602ca13a2a39ae
daniel <dc.olaru@yahoo.com>
2016-09-23 11:52:52 +0300

    GoatEatingHay

---
+++ GoatEatingHay/GoatEatingHay/GoatEatingHay.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GoatEatingHay
+{
+    [TestClass]
+    public class GoatEatingHay
+    {
+        [TestMethod]
+        public void TotalHayEatenByGoats()
+        {
+            int totalKgOfHayEaten = CalculateKgsOfHayEatenByGoat(1, 1, 10);
+            Assert.AreEqual(10, totalKgOfHayEaten);
+        }
+        [TestMethod]
+        int CalculateKgsOfHayEatenByGoat( int nrOfGoats, int nrOfDays, int kgOfHayAday)
+        {
+            
+            int totalKgOfHayEaten;
+            totalKgOfHayEaten = nrOfGoats * nrOfDays * kgOfHayAday;
+            return totalKgOfHayEaten;
+
+        }
+    }
+}
