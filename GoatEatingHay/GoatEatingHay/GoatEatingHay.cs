9e25e4eae2042b6ea1b58897c24737a30a096011
daniel <dc.olaru@yahoo.com>
2016-09-28 21:21:09 +0300

    GoatsEatingHayCorrection

--- GoatEatingHay/GoatEatingHay/GoatEatingHay.cs
+++ GoatEatingHay/GoatEatingHay/GoatEatingHay.cs
@@ -7,10 +7,16 @@ namespace GoatEatingHay
     public class GoatEatingHay
     {
         [TestMethod]
-        public void TotalHayEatenByGoats()
+        public void TotalHayEatenByOneGoatInOneDay()
         {
             int totalKgOfHayEaten = CalculateKgsOfHayEatenByGoat(1, 1, 10);
             Assert.AreEqual(10, totalKgOfHayEaten);
+        }
+        [TestMethod]
+        public void TotalHayEatenBySevenGoatsInThereeDays()
+        {
+            int totalKgOfHayEaten = CalculateKgsOfHayEatenByGoat(7, 3, 10);
+            Assert.AreEqual(210, totalKgOfHayEaten);
         }
         [TestMethod]
         int CalculateKgsOfHayEatenByGoat( int nrOfGoats, int nrOfDays, int kgOfHayAday)
