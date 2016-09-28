209876d1cab7f76d60e0c31d5f5bcb83bf0b7cfb
daniel <dc.olaru@yahoo.com>
2016-09-28 18:46:26 +0300

    SportivRoundsAndRehearsals

---
+++ SportivRounds/SportivRounds/SportivRounds.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SportivRounds
+{
+    [TestClass]
+    public class SportivRounds
+    {
+        [TestMethod]
+        public void RehearsalsForTwoRounds()
+        {
+
+            int rehearsals = CalcultateRehearsals(2);
+            Assert.AreEqual(4, rehearsals);
+        }
+        [TestMethod]
+        public void RehearsalsForTenRounds()
+        {
+
+            int rehearsals = CalcultateRehearsals(10);
+            Assert.AreEqual(100, rehearsals);
+        }
+        [TestMethod]
+        int CalcultateRehearsals(int rounds)
+        {
+            int rehearsals = (int)Math.Pow(rounds, 2);
+            return rehearsals;
+        }
+    }
+}
