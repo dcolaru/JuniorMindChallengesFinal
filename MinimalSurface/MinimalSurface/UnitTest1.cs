0e1e7384d0dd642c8257ca7bb43e4118f4e1310e
daniel <dc.olaru@yahoo.com>
2016-10-05 22:52:51 +0300

    Minimum Surface Refactoring

--- MinimalSurface/MinimalSurface/UnitTest1.cs
+++ MinimalSurface/MinimalSurface/UnitTest1.cs
@@ -8,29 +8,42 @@ namespace MinimalSurface
     {
         [TestMethod]
 
-        public void IntCoordonatesTest()
+        public void doubleCoordonatesTest()
         {
             double minimalSurface = CalculateSurface(1, 2, 3, 15, 9, 4);
-            Assert.AreEqual(0.500000, minimalSurface);
+            Assert.AreEqual(32.5, minimalSurface);
         }
 
         [TestMethod]
         
-        public void doubleCoordonatesTest()
+        public void doubleCoordonatesTest2()
         {
             double minimalSurface = CalculateSurface(1.234567, 2.234567, 3.234512, 15.234567, 9.234567, 4.234567);
-            Assert.AreEqual(0.499835, minimalSurface);
+            Assert.AreEqual(32.499977, minimalSurface);
         }
         [TestMethod]
 
         double CalculateSurface(double xa, double xb, double xc, double ya, double yb, double yc)
         {
-            double A = (double)Math.Sqrt(Math.Pow((xb - xa), 2) + Math.Pow((yb - ya), 2));
-            double B = (double)Math.Sqrt(Math.Pow((xc - xa), 2) + Math.Pow((yc - ya), 2));
-            double C = (double)Math.Sqrt(Math.Pow((xc - xb), 2) + Math.Pow((yc - yb), 2));
-            double p = (double)(A + B + C) / 2;//
-            double HeronFormula = (double)Math.Sqrt((p * ((p - A) * (p - B) * (p - C))));
-            return Math.Round(HeronFormula, 6);
+            double A = CalculateCoordonates(xa, xb,ya, yb);
+            double B = CalculateCoordonates(xa, xc, ya, yc);
+            double C = CalculateCoordonates(xc, xb, yc, ya);
+            double p = (A + B + C) / 2;
+            double minimumSurface = HeronSurface(p, A, B, C);
+            return Math.Round(minimumSurface, 6);
         }
+         double CalculateCoordonates(double xb, double xa, double ya, double yb)
+        {
+
+            return Math.Sqrt(Math.Pow((xb - xa), 2) + Math.Pow((yb - ya), 2));
+
+        }
+         double HeronSurface(double _p, double _A, double _B, double _C)
+        {
+
+            return Math.Sqrt((_p * ((_p - _A) * (_p - _B) * (_p - _C))));
+
+        }
+
     }
 }
