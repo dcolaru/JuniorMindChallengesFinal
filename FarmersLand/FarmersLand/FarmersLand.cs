fd986ca50f7c98e6b6b0ec6267ecf987b4bb7bd4
daniel <dc.olaru@yahoo.com>
2016-10-11 21:58:30 +0300

    FarmersLand

---
+++ FarmersLand/FarmersLand/FarmersLand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FarmersLand
+{
+    [TestClass]
+    public class FarmersLand
+    {
+        [TestMethod]
+        public void FirstLandSurface()
+        {
+            double land = CalculateLand(770000, 230);
+            Assert.AreEqual(592900, land);
+        }
+        public static double CalculateLand(double totalAria, double addedWidth)
+        {
+            double surfaceLength = addedWidth / 1000 - (totalAria / 1000) / (totalAria / 1000);
+
+            double firstLandAria = Math.Pow((surfaceLength * 1000), 2);
+
+            return firstLandAria;
+
+        }
+    }
+}
