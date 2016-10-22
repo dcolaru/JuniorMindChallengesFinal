69789849b01655ae78f6cd7f9ab15d3e86f5fb35
daniel <dc.olaru@yahoo.com>
2016-10-22 18:44:37 +0300

    Next Meeting At The Restaurant

---
+++ Restaurant/Restaurant/Restaurant.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Restaurant
+{
+    [TestClass]
+    public class Restaurant
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int nextDayForeeting = NextMeetingAtRestaurant(6,4);
+            Assert.AreEqual(12, nextDayForeeting);
+        }
+        int NextMeetingAtRestaurant(int a, int b)
+        {
+            int cmmmc;
+            //x=a1;y=b1;
+
+            cmmmc = (a * b) / CMMDC(a, b);
+            return cmmmc;
+        }
+        int CMMDC(int a1, int b1)
+        {
+            int r = 0;
+            while (b1 != 0)
+            {
+                r = a1 % b1;
+                a1 = b1;
+                b1 = r;
+            }
+            return a1;
+
+        }
+
+
+    }
+}
