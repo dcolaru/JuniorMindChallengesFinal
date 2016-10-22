1b2a1d7a43f2065150d41cfab734dbea8357b054
daniel <dc.olaru@yahoo.com>
2016-10-22 18:51:09 +0300

    Panagram Cases

---
+++ Panagram/Panagram/Panagra.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Panagram
+{
+    [TestClass]
+    public class Panagra
+    {
+        [TestMethod]
+        public void IsPanagram()
+        {
+            string word = getMissingLetters("Brick quiz whangs jumpy veldt fox");
+            Assert.AreEqual("true", word);
+
+        }
+        [TestMethod]
+        public void IsNotAPanagram()
+        {
+            string word = getMissingLetters("The builder builds something");
+            Assert.AreEqual("false", word);
+
+        }
+        string getMissingLetters(string sentence)
+        {
+            string alphabetLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";//alfabet
+            sentence = sentence.ToUpper();//propozitie
+
+            string message = " ";
+            for (int i = 0; i <= sentence.Length - 1; i++)
+            {
+                int j = alphabetLetters.IndexOf(sentence[i]);
+                if (j != -1)
+                {
+                    alphabetLetters = alphabetLetters.Remove(j, 1);
+
+                    message = "true";
+                }
+                else
+                {
+                    message = "false";
+                }
+            }
+            return message;
+        }
+    }
+}
