d5b5488422e33b5d506f635ba41cf22551ada3a1
daniel <dc.olaru@yahoo.com>
2016-10-22 18:54:32 +0300

    Chessboard Case

---
+++ Chessboard/Chessboard/Chessboard.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Chessboard
+{
+    [TestClass]
+    public class Chessboard
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            double squares = CheckBoard(8);
+            Assert.AreEqual(204, squares);
+        }
+        double CheckBoard(int dimension)
+        {
+            double numberOfSquares = 0;
+            for (int i = 0; i <= dimension; dimension--)
+            {
+                numberOfSquares += Math.Pow(dimension, 2);
+            }
+            return numberOfSquares;
+        }
+    }
+}
