b31f6327c1f42e6ad803fba77335e0080d078ffd
daniel <dc.olaru@yahoo.com>
2016-10-25 21:07:31 +0300

    Excel Column One Letter Column

---
+++ Excel/Excel/ExcelColumn.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Excel
+{
+    [TestClass]
+    public class ExcelColumn
+    {
+        [TestMethod]
+        public void OneLetter()
+        {
+            Assert.AreEqual("B", Excel(2));
+        }
+        
+        string Excel(int number)
+        {
+
+
+            string convertedNumber = "";
+            
+
+            while (number > 0)
+            {
+                number--;
+                convertedNumber = ExcelFindColumn((number)%26) + convertedNumber;
+                number = number  / 26;
+
+            }
+            return convertedNumber;
+        }
+        public string ExcelFindColumn(int number)
+        {
+            return ((char)('A' + number)).ToString();
+        }
+    }
+}
